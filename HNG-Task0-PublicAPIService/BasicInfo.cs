55be1356b78331df429058d1240bbdead315f014
Promise30 <promiseadeagbo@gmail.com>
2025-01-29 23:51:50 +0100

    Add project files.

---
+++ HNG-Task0-PublicAPIService/BasicInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace HNG_Task0_PublicAPIService
+{
+    public class BasicInfo
+    {
+        public string Email { get; set; }
+        public string Time { get; set; } 
+        public string GetCurrentIso8601Timestamp()
+        {
+            return DateTime.UtcNow.ToString("o");
+        }
+        public string GithubUrl { get; set; } = "";
+    }
+}
---
+++ HNG-Task0-PublicAPIService/Controllers/BasicInfoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HNG_Task0_PublicAPIService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasicInfoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var basicInfo = 
+            return Ok(basicInfo);
+        }
+    }
+}
